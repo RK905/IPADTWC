fa91081cb746348ff65b1f21d53f585e98f016a6
RK905 <mr.rayenk@gmail.com>
2015-10-27 15:59:06 -0400

    Initial Commit

---
+++ IPADTWC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IPADTWC
+//
+//  Created by Rayen Kamta on 10/27/15.
+//  Copyright © 2015 Geeksdobyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
